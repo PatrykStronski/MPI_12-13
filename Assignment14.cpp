2bccedcc344c0a6a713dcd3900e9536b591cb438
Patrik Stronski <p.stronski13@gmail.com>
2021-12-23 00:27:39 +0300

    done

--- Assignment14.cpp
+++ Assignment14.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "mpi.h"
+
 #define n 1000
 
 void smod5(void *a, void *b, int *l, MPI_Datatype *type) {
@@ -8,13 +9,23 @@ void smod5(void *a, void *b, int *l, MPI_Datatype *type) {
 		((int*)b)[i] = (((int*)a)[i] + ((int*)b)[i]) % 5;
 }
 
+void biggest_numb(void *a, void *b, int *l, MPI_Datatype *type) {
+	((int*)b)[0] = ((int*)a)[0];
+	for (int i = 1; i < *l; i++) {
+		if (((int*)a)[i] > ((int*)b)[0]) {
+			((int*)b)[0] = ((int*)a)[i];
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	int rank, size, i;
 	int a[n];
 	int b[n];
+	int m[1];
 	MPI_Init(&argc, &argv);
-	MPI_Op op;
+	MPI_Op op, max;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	for (i = 0; i < n; i++) a[i] = i + rank + 1;
@@ -23,5 +34,11 @@ int main(int argc, char **argv)
 	MPI_Reduce(a, b, n, MPI_INT, op , 0, MPI_COMM_WORLD);
 	MPI_Op_free(&op);
 	if (rank == 0) printf("b[0] = %d\n", b[0]);
+
+	MPI_Op_create(&biggest_numb, 1, &max);
+	MPI_Reduce(a, m, n, MPI_INT, max , 0, MPI_COMM_WORLD);
+	MPI_Op_free(&max);
+	if (rank == 0) printf("max is %d\n", m[0]);
+
 	MPI_Finalize();
 }
