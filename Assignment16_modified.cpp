1c24b3b0e61eadde07677f30a91ac7e0bd71b925
Patrik Stronski <p.stronski13@gmail.com>
2021-12-24 03:21:56 +0300

    a16-17

--- Assignment16_modified.cpp
+++ Assignment16_modified.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	MPI_Comm comm_revs;
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_split(MPI_COMM_WORLD, 1, size - rank, &comm_revs);
+	MPI_Comm_split(MPI_COMM_WORLD, rank%3, size - rank, &comm_revs);
 	MPI_Comm_rank(comm_revs, &rank1);
 	cout<<"rank = "<<rank<<"; rank1 = "<<rank1<<endl;
 	MPI_Comm_free(&comm_revs);
--- Assignment17.cpp
+++ Assignment17.cpp
@@ -1,4 +1,14 @@
 #include "mpi.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print_status(int rank, float* a, char* b) {
+	cout<<" For rank "<<rank<<" a is ["<<a[0]<<","<<a[1]<<","<<a[2]<<","<<a[3]<<","<<a[4]<<","<<a[5]<<","<<a[6]<<","<<a[7]<<","<<a[8]<<","<<a[9]<<"] and b is ["<<
+	b[0]<<","<<b[1]<<","<<b[2]<<","<<b[3]<<","<<b[4]<<","<<b[5]<<","<<b[6]<<","<<b[7]<<","<<b[8]<<","<<b[9]<<"]"<<endl;
+}
+
 int main(int argc, char **argv)
 {
 	int size, rank, position, i;
@@ -12,6 +22,7 @@ int main(int argc, char **argv)
 		if (rank == 0) b[i] = 'a';
 		else b[i] = 'b';
 	}
+	print_status(rank, a, b);
 	position = 0;
 	if (rank == 0) {
 		MPI_Pack(a, 10, MPI_FLOAT, buf, 100, &position, MPI_COMM_WORLD);
@@ -24,5 +35,6 @@ int main(int argc, char **argv)
 		MPI_Unpack(buf, 100, &position, a, 10, MPI_FLOAT, MPI_COMM_WORLD);
 		MPI_Unpack(buf, 100, &position, b, 10, MPI_CHAR, MPI_COMM_WORLD);
 	}
+	print_status(rank, a, b);
 	MPI_Finalize();
 }
